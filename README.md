a1ac105b7013c2408b86bd3fea7e91a45bf4aab1
OZIELE RODRIGUES DE OLIVEIRA <oziele.rodrigues@academico.ifrn.edu.br>
2022-07-18 09:16:12 -0300

    siglas

---
+++ README.md
@@ -0,0 +1,5 @@
+# calc_notas_IF_reactNative
+
+Este projeto visa sanar duvidas com relação ao calculo de notas do IFRN.  
+Pode-se ver quanto preccisa alcançar de notas para poder se concluir a disciplina, quanto, precisará, caso fique na prova final.  
+
--- __tests__/Siglas.js
+++ __tests__/Siglas.js
@@ -1,3 +1,4 @@
+
 import { render, fireEvent} from "@testing-library/react-native";
 import Home from "../components/Home";
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper")
@@ -6,16 +7,16 @@ describe("teste de Inicialização e Exibição", ()=>{
     it("Componentes de Index", ()=>{
         const {getByText, getByTestId} = render(<Home />)
 
-        expect (getByText("DG")).toBeTruthy()
+        expect (getByText("")).toBeTruthy()
         expect(getByTestId("btnindex")).toBeTruthy();
     })
 })
 describe("Testes de Navegação", ()=>{
     it ("Navegação de Index para Dobro",()=>{
-        const{getByText, getByTestId} = render(<App />)
+        const{getByText, getByTestId} = render(<Home />)
 
-        const botao = getByTestId("btnindex")
+        const botao = getByTestId("btn")
         fireEvent.press(botao)
-        expect(getByText("Tela Dobro")).toBeTruthy();
+        expect(getByText("")).toBeTruthy();
     })
 })
