daeb9a5af2f713198784040e5e3ff3649d84fd08
OzieleOliveira <100812251+OzieleOliveira@users.noreply.github.com>
2022-07-25 17:50:34 -0300

    update significaodo-test

--- __tests__/Singnificado-test.js
+++ __tests__/Singnificado-test.js
@@ -4,13 +4,24 @@ import Significado from "../components/Significado";
 //import App from "../App"
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper")
 
+describe("teste de Snapshot", ()=>{
+    it("Componentes de Significado", ()=>{
+        const significado = renderer.create(<Significado />).toJSON();
+        expect(significado).toMatchSnapshot();
 
+    })
+})
 
 describe("Teste de Inicialização e Exibição", ()=>{
     it("Componentes de Significado", ()=>{
         const {getByText, getByTestId} = render(<Significado/>)
 
-        expect(getByTestId("Teste")).toBeTruthy();
+        //expect(getByTestId("Teste")).toBeTruthy();
         //expect(getByTestId("btext")).toBeTruthy();
+        
+        const dg = getByTestId('btn')
+        fireEvent.press(dg)
+        expect(getByText('DG')).toBeDefined()
+        
     })
 })
